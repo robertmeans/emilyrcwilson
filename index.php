59e81c59089a965143d1ce8783fb0afea82e572a
Robert Means <robert@robertmeans.com>
2025-02-02 13:03:52 -0700

    Update index.php

--- index.php
+++ index.php
@@ -67,13 +67,13 @@ require '_includes/head.php'; ?>
     <div id="toggleAbtPg"><i class="fas fa-plus"></i> About this page</div>
     <div id="abtPg">
       <p>The above search includes 1,327 pronunciations that Emily Wilson, Translator of <em><a href="https://www.emilyrcwilson.com/the-odyssey" target="_blank">The Odessey</a></em> and <em><a href="https://www.emilyrcwilson.com/the-iliad-sept-2023" target="_blank">The Iliad</a></em>, has painstakingly provided on <a href="https://emilyrcwilson.com" target="_blank">her website</a>.</p>
-      <p>I made this search so that I could find pronunciations quickly, with minimal interruptions, while I read. This was designed with my <a href="https://www.apple.com/by/iphone-13/" target="_blank">iPhone 13</a> mini as the basis for keeping the search results "above the fold" - in other words, to keep the results visible while the keypad is also occupying screen real estate. My apologies if you are on a smaller screen or if <em>anything</em> isn't just exactly perfect. <i class="far fa-smile"></i></p>
+      <p>I made this search so that I could find pronunciations quickly, with minimal interruptions, while I read. This was designed with my <a href="https://www.apple.com/by/iphone-13/" target="_blank">iPhone 13</a> mini as the basis for keeping the search results "above the fold" - in other words, to keep the results visible while the keypad is also occupying screen real estate. My apologies if you are on a smaller viewport or if <em>anything</em> isn't just exactly perfect. <i class="far fa-smile"></i></p>
       <p>While each file, individually, is relatively small, cumulatively they add up to a fair amount of data. In order to manage the page load, the mp3 files are only accessed on a per-click basis and are neither preloaded nor cached. This keeps the page small and quick to load, however, the tradeoff is that an active Internet connection is required for consistently smooth playbacks.</p>
       <p><a id="closeAbtPg">Close</a></p>
     </div>
     <h1 class="ttext">Emily Wilson</h1>
     <h2 class="stext">Translator of The <em>Iliad</em> and the <em>Odyssey</em></h2>
-    <p>The content below will take you to Emily Wilson's website where you can find additional resources about both of her world-renowned translations, learn about the author and find additional resources.</p>
+    <p>The content below will take you to <a href="https://emilyrcwilson.com" target="_blank">Emily Wilson's website</a> where you can find additional resources about both of her world-renowned translations, learn about the author and find additional resources.</p>
 
     <div class="books">
       <div class="book">
